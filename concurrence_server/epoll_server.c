0ae0256b99281d0d4e9c04b915f705dcfd9706ef
ch <304831714@qq.com>
2020-07-05 20:50:20 -0700

    c

---
+++ concurrence_server/epoll_server.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <sys/epoll.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+
+int main()
+{
+	int lfd = socket(PF_INET, SOCK_STREAM, 0);
+	struct sockaddr_in server_addr;
+	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server_addr.sin_port = htons(54325);
+	server_addr.sin_family = AF_INET;
+
+	int flag = 1;
+	setsockopt(lfd, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(flag));
+	int ret = bind(lfd, (struct sockaddr *)&server_addr,sizeof(server_addr));
+	if(ret == -1)
+	{
+		perror("bind fail.");
+		exit(1);
+	}
+
+	listen(lfd, 36);
+
+	struct epoll_event ev;
+	ev.events = EPOLLIN;
+	ev.data.fd = lfd;
+	int epfd = epoll_create(2000);
+	epoll_ctl(epfd, EPOLL_CTL_ADD, lfd, &ev);
+
+	struct epoll_event all_event[2000];
+	struct sockaddr_in client_addr;
+	socklen_t client_len = sizeof(client_addr);
+	printf("Start epoll wait...\n");
+
+		
+	while(1){
+		int ready = epoll_wait(epfd, all_event, sizeof(all_event) / sizeof(struct epoll_event), -1);	
+		for(int i = 0; i < ready; ++i){
+			int fd = all_event[i].data.fd;
+			if(fd == lfd){
+				int cfd = accept(lfd, (struct sockaddr *)&client_addr, &client_len);
+				if(cfd == -1)
+				{
+					perror("accept error");
+					exit(1);
+				}
+				
+				struct epoll_event temp;
+				temp.data.fd = cfd;
+				temp.events = EPOLLIN;
+				epoll_ctl(epfd, EPOLL_CTL_ADD, cfd, &temp);
+				char ip[64];
+				printf("client IP: %s , Port: %d\n",
+								inet_ntop(AF_INET, &client_addr.sin_addr.s_addr,ip, sizeof(ip)), ntohs(client_addr.sin_port));
+
+			}
+			else{
+				if(!all_event[i].events & EPOLLIN)
+				{
+					continue;
+				}
+
+				char buff[1024] = {0};
+				int len = recv(fd, buff, sizeof(buff),0);
+				if(len == -1){
+					perror("recv error");
+					close(fd);
+				}
+				else if(len == 0){
+					printf("client disconnected...\n");
+					close(fd);
+					epoll_ctl(epfd, EPOLL_CTL_DEL, fd, NULL);
+				}
+				else{
+					printf("recv buf: %s\n", buff);
+					write(fd, buff, sizeof(buff));
+				}
+			}
+		}
+	}
+
+	return 0;
+}
